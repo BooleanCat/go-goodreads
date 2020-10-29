38a23979ae4e0dab019ec1ee82f2b33a12f1b16c
Tom Godkin <tomgodkin@pm.me>
2020-10-29 12:50:40 +0000

    Add missing docstrings

--- httputils/httputils.go
+++ httputils/httputils.go
@@ -5,11 +5,13 @@ import (
 	"time"
 )
 
+// DripLimitTransport rate limits requests by regular intervals.
 type DripLimitTransport struct {
 	delegate http.RoundTripper
 	ticker   *time.Ticker
 }
 
+// DripLimit creates a new DripLimitTransport. The caller must stop the time.Ticker.
 func DripLimit(delegate http.RoundTripper, ticker *time.Ticker) DripLimitTransport {
 	return DripLimitTransport{
 		delegate: delegate,
@@ -17,6 +19,7 @@ func DripLimit(delegate http.RoundTripper, ticker *time.Ticker) DripLimitTranspo
 	}
 }
 
+// RoundTrip implements http.RoundTripper.
 func (client DripLimitTransport) RoundTrip(request *http.Request) (*http.Response, error) {
 	<-client.ticker.C
 	return client.delegate.RoundTrip(request)
