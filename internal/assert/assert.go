6b6f31e71a98ae693741adb548648e573d8d7c2d
Tom Godkin <tomgodkin@pm.me>
2020-10-30 10:40:36 +0000

    Add missing docstrings

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -7,24 +7,28 @@ import (
 	"testing"
 )
 
+// Nil succeeds when the value is nil.
 func Nil(t *testing.T, v interface{}) {
 	if v != nil {
 		t.Fatalf(`expected "%v" to be nil`, v)
 	}
 }
 
+// Equal succeeds when the two values are equal.
 func Equal(t *testing.T, v, w interface{}) {
 	if !reflect.DeepEqual(v, w) {
 		t.Fatalf(`expected "%v" to equal "%v"`, v, w)
 	}
 }
 
+// EndsWith succeeds when the input string ends with the given suffix.
 func EndsWith(t *testing.T, s, u string) {
 	if !strings.HasSuffix(s, u) {
 		t.Fatalf(`expected "%s" to end with "%s"`, s, u)
 	}
 }
 
+// ErrorMatches succeeds when the error string matches the given pattern.
 func ErrorMatches(t *testing.T, err error, pattern string) {
 	if err == nil {
 		t.Fatal("expected err to have occurred")
@@ -35,12 +39,14 @@ func ErrorMatches(t *testing.T, err error, pattern string) {
 	}
 }
 
+// DoesNotContainSubstring succeeds when the input string doesn't contain the given substring.
 func DoesNotContainSubstring(t *testing.T, x, y string) {
 	if strings.Contains(x, y) {
 		t.Fatalf(`expected "%s" not to contain "%s"`, x, y)
 	}
 }
 
+// True succeeds when the input is true.
 func True(t *testing.T, v interface{}) {
 	if !reflect.DeepEqual(v, true) {
 		t.Fatal("expected true")
